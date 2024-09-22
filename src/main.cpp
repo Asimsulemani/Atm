5161249ceab612ad14a3e2f23e269a31c76a5cba
root <root@localhost>
2024-09-23 03:02:14 +0500

    auto publish 1727042534493

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream.h > 
+
+#include < conio.h > 
+
+void main ()
+{
+int atm;
+clrscr();
+cout<< "Inter your password please ";
+cin atm;_
+if(atm==10123 )
+{ 
+cout << You can access Your balance ";
+}
+getch ();
